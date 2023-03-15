0dd54440c6f9b15e1b048ae1bb91e5ef119bffe4
AbelHuerta <97577960+AbelHuerta@users.noreply.github.com>
2023-03-15 13:11:00 -0500

    Add files via upload

---
+++ EJEMPLO4_TIMER_NORMAL_MODE/EJEMPLO_1_TIMER_NORMAL_MODE/librerias/Timers_avr.h
@@ -0,0 +1,17 @@
+/*
+ * Timers_avr.h
+ *
+ * Created: 14/05/2022 15:47:48
+ *  Author: abelh
+ */ 
+
+
+#ifndef TIMERS_AVR_H_
+#define TIMERS_AVR_H_
+
+#define  Valor_100ms	40536
+void Timer1_init();
+
+
+
+#endif /* TIMERS_AVR_H_ */
